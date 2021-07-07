471208741dda4d9f0efbb31c9a46dbb2c5b28c2b
pshivpuje <prathamesh-ashok.shivpuje@capgemini.com>
2021-07-07 23:49:10 +0530

    commit done

--- Prathamesh/src/main/java/Swapnil.java
+++ Prathamesh/src/main/java/Swapnil.java
@@ -3,7 +3,9 @@ public class Swapnil {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("HEllo World");
+			System.out.println("HEllo World India");
+			System.out.println("HEllo World china");
+			System.out.println("HEllo World korea ");
 	}
 
 }
