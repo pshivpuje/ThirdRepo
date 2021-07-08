a6fa6a4ec366d34de5eba167c89c2228af767946
pshivpuje <prathamesh-ashok.shivpuje@capgemini.com>
2021-07-08 09:58:12 +0530

    Validation done

---
+++ Prathamesh/src/main/java/Validation.java
@@ -0,0 +1,11 @@
+
+public class Validation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the Username:- ");
+		System.out.println("Enter the Password:- ");
+
+	}
+
+}
